320abdf49c934df0b73c1d517d1de9f672c8c680
James Dixon <jamesdixon117@gmail.com>
2019-04-28 21:57:28 +1000

    Added entry point for javascript code.

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+function main() {
+    // Entry point.
+
+    // Create handlers for canvas.
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.rect(20, 40, 50, 50);
+    ctx.fillStyle = "#FF0000";
+    ctx.fill();
+    ctx.closePath()
+}
