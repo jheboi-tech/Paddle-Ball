e5ec217dfb6226c0b5ab3c4ccf009c69e17a1475
James Dixon <jamesdixon117@gmail.com>
2019-04-28 22:44:13 +1000

    Added mechanism to wait for page to load.
    If script is run prior to page loading, the script will not be able to
    find the canvas element. Added waiting event to allow script to start
    when the page has loaded.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,42 @@
-function main() {
-    // Entry point.
+// Define the frames_per_seconds and related
+// constants.
+const frames_per_second = 30;
+const seconds_per_frame = 1 / frames_per_second;
+const milliseconds_per_frame = seconds_per_frame / 1000;
 
-    // Create handlers for canvas.
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
+// Create the canvas object as a global variable.
 
-    ctx.beginPath();
-    ctx.rect(20, 40, 50, 50);
-    ctx.fillStyle = "#FF0000";
-    ctx.fill();
-    ctx.closePath()
+var canvas = null;
+var ctx = null;
+var frame = 0;
+
+/**
+ * Function draws all the elements to the
+ * frame.
+ */
+function draw() {
+    console.log("Draw frame" + frame++);
 }
+
+/**
+ * This main function is the function that kicks
+ * off the application.
+ */
+function main() {
+    // Kick off the frame drawing process.
+    // This will cause the function "draw"
+    // to be run every "milliseconds_per_frame".
+    setInterval(draw, milliseconds_per_frame);
+}
+
+/**
+ * Wait for elements to be drawn to the page prior to
+ * creating the canvas handlers. Since this script is
+ * called prior to the canvas being created, we need to
+ * wait for the canvas to be drawn.
+ */
+window.addEventListener('load', function () {
+    canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext("2d");
+    main();
+}, false);
